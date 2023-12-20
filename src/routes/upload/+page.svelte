05748f27ec27fe5ca360554b842bfcef1848c5fd
Daniel Riccio <ricciodaniel98@gmail.com>
2023-12-19 19:21:38 -0500

    wip: add validation

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,70 +1,109 @@
 <script lang="ts">
+	import Alert from '../../components/Alert.svelte';
 	import { FileDropzone } from '@skeletonlabs/skeleton';
 	import { FileCheck, FileUp } from 'lucide-svelte';
-	import type { CreateSubtitleInput } from '../../types/CreateSubtitleInput';
 	import { FeatureType } from '../../types/FeatureTypes';
 	import { createSubtitle } from '../../api/createSubtitle';
 	import { formatFileSize } from '../../helpers/formatFileSize';
-	import type { Subtitle } from '../../types/Subtitle';
-	import { debounce } from 'lodash';
 
 	// Constants
-	let iconSize = '4rem';
+	const ICON_SIZE = '4rem';
 
 	// State variables
+	let alertVisible = false;
+	let alert = {
+		title: '',
+		message: ''
+	};
 	let isFormSubmitted = false;
-	let loading = false;
-	let featureType = FeatureType.Episode;
-	let fileInput: File | undefined = undefined;
+	let isFormValid = false;
+	let isLoading = false;
+	let selectedFeatureType = FeatureType.Episode;
 
-	// Input data
-	let input = {
+	// Form data model
+	let uploadForm = {
 		imdbId: '',
 		comments: '',
 		language: 'en',
 		releaseName: '',
 		episodeNumber: '',
 		seasonNumber: '',
-		file: undefined as File | undefined
+		file: undefined as File | undefined,
+		featureType: FeatureType.Episode
 	};
 
-	// Event handler for input change
-	const handleInputChange = debounce((event: Event) => {
-		const target = event.target as HTMLInputElement;
-		featureType = target.value as FeatureType;
-	}, 300);
+	let validationErrors = {
+		imdbId: false,
+		releaseName: false,
+		language: false,
+		seasonNumber: false,
+		episodeNumber: false,
+		file: false
+	};
 
-	// Event handler for form submission
+	function validateForm() {
+		// Reset validation errors
+		validationErrors = {
+			imdbId: !uploadForm.imdbId,
+			releaseName: !uploadForm.releaseName,
+			file: !uploadForm.file,
+			language: false,
+			seasonNumber: uploadForm.featureType === FeatureType.Episode && !uploadForm.seasonNumber,
+			episodeNumber: uploadForm.featureType === FeatureType.Episode && !uploadForm.episodeNumber
+		};
+		isFormValid = !Object.values(validationErrors).some(Boolean);
+		return isFormValid;
+	}
+
 	async function handleSubmit() {
-		loading = true;
+		validateForm();
 
-		if (fileInput) {
-			input.file = fileInput;
-			const formData = createFormData(input);
-			await createSubtitle(formData);
+		//debug
+		console.log(uploadForm);
+
+		// Check if form is valid
+		if (!isFormValid) {
+			isLoading = false;
+			return;
 		}
 
-		loading = false;
+		isLoading = true;
+
+		try {
+			const formData = createFormData();
+			await createSubtitle(formData);
+		} catch (error) {
+			const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+			setAlert('Upload Failed', errorMessage);
+			isLoading = false;
+			return;
+		}
+
+		isLoading = false;
 		isFormSubmitted = true;
 	}
 
-	// Helper function to create FormData
-	function createFormData(input: CreateSubtitleInput): FormData {
+	function createFormData(): FormData {
 		const formData = new FormData();
-
-		// TODO: Add validation
-
-		formData.append('imdbId', input.imdbId || '');
-		formData.append('comments', input.comments || '');
-		formData.append('language', input.language || '');
-		formData.append('releaseName', input.releaseName || '');
-		formData.append('episodeNumber', input.episodeNumber || '');
-		formData.append('seasonNumber', input.seasonNumber || '');
-		formData.append('file', input.file as File);
-
+		Object.entries(uploadForm).forEach(([key, value]) => {
+			if (value) formData.append(key, value);
+		});
 		return formData;
 	}
+
+	function setAlert(title: string, message: string) {
+		alertVisible = true;
+		alert.title = title;
+		alert.message = message;
+	}
+
+	//file property on input element doesn't work
+	function onFileDropzoneChangeHandler(e: Event): void {
+		uploadForm.file = (e.target as HTMLInputElement).files?.[0];
+	}
 </script>
+
+<Alert bind:visible={alertVisible} bind:title={alert.title} bind:message={alert.message} />
 
 {#if !isFormSubmitted}
 	<div class="card p-6">
@@ -75,87 +114,107 @@
 			<form on:submit|preventDefault={handleSubmit}>
 				<label class="label">
 					<span>IMDb ID</span>
-					<input class="input" type="text" placeholder="tt1234567" bind:value={input.imdbId} />
+					<input
+						class="input variant-form-material {validationErrors.imdbId ? 'input-error' : ''}"
+						type="text"
+						placeholder="tt1234567"
+						bind:value={uploadForm.imdbId}
+					/>
+					{#if validationErrors.episodeNumber}
+						<span class="validation-error">Please enter a valid IMDb ID</span>
+					{/if}
 				</label>
 
 				<label class="label">
 					<span>Release Name</span>
 					<input
-						class="input"
+						class="input variant-form-material {validationErrors.releaseName ? 'input-error' : ''}"
 						type="text"
 						placeholder="Raised.by.Wolves.2020.S02E07.1080p.WEB.H264-CAKES[TGx]"
-						bind:value={input.releaseName}
+						bind:value={uploadForm.releaseName}
 					/>
+					{#if validationErrors.episodeNumber}
+						<span class="validation-error">Please enter a valid release name</span>
+					{/if}
 				</label>
 
 				<label class="label">
 					<span>Comments</span>
-					<input class="input" type="text" placeholder="CAKES, TGx" bind:value={input.comments} />
+					<input
+						class="input variant-form-material"
+						type="text"
+						placeholder="CAKES, TGx"
+						bind:value={uploadForm.comments}
+					/>
 				</label>
 
 				<label class="label">
 					<span>Language</span>
-					<select class="select" bind:value={input.language}>
+					<select class="select variant-form-material" bind:value={uploadForm.language}>
 						<option value="es">Spanish</option>
 						<option value="en">English</option>
 					</select>
 				</label>
 
-				<div class="space-y-2">
-					<label class="flex items-center space-x-2">
-						<input
-							class="radio"
-							type="radio"
-							checked
-							name="radio-direct"
-							value={FeatureType.Episode}
-							bind:group={featureType}
-							on:change={handleInputChange}
-						/>
-						<p>TV Show</p>
-					</label>
-					<label class="flex items-center space-x-2">
-						<input
-							class="radio"
-							type="radio"
-							name="radio-direct"
-							value={FeatureType.Movie}
-							bind:group={featureType}
-							on:change={handleInputChange}
-						/>
-						<p>Movie</p>
-					</label>
-				</div>
-				{#if featureType === 'episode'}
+				<label class="label">
+					<span>Feature Type</span>
+					<select class="select variant-form-material" bind:value={uploadForm.featureType}>
+						<option value={FeatureType.Episode}>TV Show</option>
+						<option value={FeatureType.Movie}>Movie</option>
+					</select>
+				</label>
+
+				{#if selectedFeatureType === 'episode'}
 					<label class="label">
 						<span>Season Number</span>
-						<input class="input" type="text" placeholder="14" bind:value={input.seasonNumber} />
+						<input
+							class="input variant-form-material {validationErrors.seasonNumber
+								? 'input-error'
+								: ''}"
+							type="text"
+							placeholder="14"
+							bind:value={uploadForm.seasonNumber}
+						/>
+						{#if validationErrors.episodeNumber}
+							<span class="validation-error">Please enter a valid season number.</span>
+						{/if}
 					</label>
 					<label class="label">
 						<span>Episode Number</span>
-						<input class="input" type="text" placeholder="8" bind:value={input.episodeNumber} />
+						<input
+							class="input variant-form-material {validationErrors.episodeNumber
+								? 'input-error'
+								: ''}"
+							type="text"
+							placeholder="8"
+							bind:value={uploadForm.episodeNumber}
+						/>
+						{#if validationErrors.episodeNumber}
+							<span class="validation-error">Please enter a valid episode number.</span>
+						{/if}
 					</label>
 				{/if}
 
 				<FileDropzone
 					name="files"
-					bind:file={fileInput}
 					accept=".srt"
 					multiple={false}
-					class={fileInput !== undefined ? 'border-2 border-dashed' : 'border-solid'}
+					on:change={(e) => onFileDropzoneChangeHandler(e)}
+					class={uploadForm.file !== undefined ? 'border-2 border-dashed' : 'border-solid'}
 				>
 					<div class="upload-dropzone-icon" slot="lead">
-						{#if fileInput === undefined}
-							<FileUp size={iconSize} />
+						{#if uploadForm.file === undefined}
+							<FileUp size={ICON_SIZE} />
 						{:else}
-							<FileCheck size={iconSize} />
+							<FileCheck size={ICON_SIZE} />
 						{/if}
 					</div>
 					<svelte:fragment slot="message">
-						{#if fileInput !== undefined}
+						{#if validationErrors.file}
+							<div class="validation-error">Please select a file to upload.</div>
+						{:else if uploadForm.file !== undefined}
 							<div>
-								File: {fileInput.name} |
-								{formatFileSize(fileInput.size)}
+								File: {uploadForm.file.name} | {formatFileSize(uploadForm.file.size)}
 							</div>
 						{:else}
 							Upload a file or drop it here
@@ -165,7 +224,7 @@
 				</FileDropzone>
 
 				<button type="submit" class="btn variant-filled">
-					{#if loading}
+					{#if isLoading}
 						<span>Loading...</span>
 					{:else}
 						<span>Submit</span>
@@ -195,7 +254,7 @@
 		justify-content: center;
 		align-items: center;
 	}
-	.space-y-2 {
-		margin-bottom: 1rem;
+	.validation-error {
+		color: rgb(var(--color-error-500));
 	}
 </style>
